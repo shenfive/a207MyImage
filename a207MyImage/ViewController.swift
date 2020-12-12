bd728fcccd1b8953dabbe9db019439ee42090d09
Shen Jun Wu <shenfive@gmail.com>
2020-12-12 11:11:45 +0800

    netWork

--- a207MyImage/ViewController.swift
+++ a207MyImage/ViewController.swift
@@ -12,10 +12,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let newView = MyView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+        newView.clipsToBounds = true
         self.view.addSubview(newView)
+        print(getNetworkStatus())
         
         
     }
+    
+    func getNetworkStatus()->Bool{
+        if Reachability(hostName: "www.apple.com")?.currentReachabilityStatus().rawValue == 0 {
+            return false
+        }else{
+            return true
+        }
+    }
+
 
 
 }
